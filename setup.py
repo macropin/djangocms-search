396ebfb7a189b61f61883a48f77ead73a002b315
garmoncheg <garmon1@gmail.com>
2015-08-12 15:14:35 +0300

    fixed for 0.3

--- setup.py
+++ setup.py
@@ -8,6 +8,17 @@ from pip.req import parse_requirements
 install_reqs = parse_requirements('requirements.txt')
 requirements = [str(ir.req) for ir in install_reqs]
 
+template_patterns = [
+    'templates/*.html',
+    'templates/*/*.html',
+    'templates/*/*/*.html',
+]
+
+package_data = dict(
+    (package_name, template_patterns)
+    for package_name in packages
+)
+
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Environment :: Web Environment',
@@ -33,6 +44,7 @@ setup(
     author_email='garmon1@gmail.com',
     url='https://github.com/garmoncheg/djangocms-search',
     packages=find_packages('djangocms_search'),
+    package_data=package_data,
     install_requires=requirements,
     license='MTI',
     platforms=['OS Independent'],
