7b7204ceb45327b98d30051ac8a8f340554ce391
Andrew Cutler <macropin@gmail.com>
2019-03-13 19:21:04 +1100

    Fix issue with parse_requirements and pip versions Closes https://github.com/garmoncheg/djangocms-search/issues/1

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@
 from setuptools import setup
 from setuptools import find_packages
 from djangocms_search import __version__
-from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 import pip
 
 requirements = [
